39aab5ebdf6c8ae1c3d20f388251eb93a7f897c4
qishione <sal.su@qq.com>
2011-08-27 17:33:12 +0800

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mygit::Application.config.secret_token = '94a0cbdd266a7829d2dc9df025a6427e2400d5bc79df0b3c7991c462c1b32eeab66988fd3fba10702e77697353ff40c3c6b00f4fd8230b44f81ae94fe9cc2217'
